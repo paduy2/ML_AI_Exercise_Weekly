de5e087a28f00046a55a29301fec380e2ff18d6c
paduy2 <paduy2@tma.com.vn>
2024-06-17 11:29:34 +0700

    Add exercise week 4

---
+++ Lesson4_EX1/classification.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+# from ydata_profiling import ProfileReport
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("csgo.csv")
+# profile = ProfileReport(data, title="Match Report", explorative="True")
+# profile.to_file("report/html")
+
+# split data
+target = "result"
+non_scale = "map"
+x = data.drop([target,"day","month","year","date","wait_time_s","match_time_s","team_a_rounds","team_b_rounds"], axis=1)
+y = data[target]
+
+# map_values = x['map'].unique()
+# print (map_values)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=42)
+
+# Separate the DataFrame into the columns to be scaled and the column to exclude
+x_train_non_scale = x_train[non_scale]
+x_train_to_scale = x_train.drop(columns=[non_scale], axis=1)
+
+x_test_non_scale = x_test[non_scale]
+x_test_to_scale = x_test.drop(columns=[non_scale], axis=1)
+# Preprocess data
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train_to_scale)
+x_test = scaler.transform(x_test_to_scale)
+
+#Prepare params for randomforest model
+params = {
+    "n_estimators": [50, 100],
+    "criterion": ["gini", "entropy", "log_loss"],
+    "max_depth": [None, 2, 5],
+    "min_samples_split": [2, 5, 10]
+}
+
+# model = SVC()
+model = LogisticRegression(multi_class='auto')
+# model = RandomForestClassifier()
+model.fit(x_train, y_train)
+
+# model = GridSearchCV(RandomForestClassifier(random_state=100), param_grid=params, scoring="precision", cv=6, verbose=2,n_jobs=1)
+# model.fit(x_train, y_train)
+# print(model.best_score_)
+# print(model.best_params_)
+
+#Analyze 
+y_predict = model.predict(x_test)
+# Adjust the threshold for each class
+
+#Show Result
+# for i,j in zip(y_predict, y_test):
+#   print ("Predict: {} . Actual: {}".format(i,j))
+print(classification_report(y_test, y_predict))
+# print (confusion_matrix(y_test,y_predict))
+# cm = np.array(confusion_matrix(y_test,y_predict))
+# confusion = pd.DataFrame(cm, index=["Win", "Tie", "Lost"], columns=["Win", "Tie", "Lost"])
+# sns.heatmap(confusion)
+# plt.show()
